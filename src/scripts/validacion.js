41ca9156831170671e00bbf98ef87f320b6a6bd4
Jennifer Arabel <jennifer.arabel@gmail.com>
2024-07-13 01:16:36 -0300

    Modified validacion.js

--- src/scripts/validacion.js
+++ src/scripts/validacion.js
@@ -2,10 +2,11 @@
 function validarFormulario() {
     var nombre = document.forms["form"]["nombre"].value;
     var email = document.forms["form"]["email"].value;
+    var asunto = document.forms["form"]["asunto"].value;
     var mensaje = document.forms["form"]["mensaje"].value;
 
     // Validación básica de campos obligatorios
-    if (nombre === "" || email === "" || mensaje === "") {
+    if (nombre === "" || email === "" || mensaje === "" || asunto === "") {
         alert("Por favor, completa todos los campos.");
         return false;
     }
@@ -16,18 +17,30 @@ function validarFormulario() {
         return false;
     }
 
+    // Validación de longitud máxima de campos
+    if (nombre.length > 50 || asunto.length > 50) {
+        alert("Los campos no deben exceder los 50 caracteres.");
+        return false;
+    }
+    if (mensaje.length > 300) {
+        alert("El mensaje no debe exceder los 300 caracteres.");
+        return false;
+    }
+ 
+    // Función para validar formato de email
+    function validarEmail(email) {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
+ 
+    // Event listener para el envío del formulario
+    document.querySelector("form").addEventListener("submit", function(event) {
+        if (!validarFormulario()) {
+            event.preventDefault(); // Evitar que se envíe el formulario si la validación falla
+        }
+    });
+ 
+
     return true;
 }
 
-// Función para validar formato de email
-function validarEmail(email) {
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
-}
-
-// Event listener para el envío del formulario
-document.querySelector("form").addEventListener("submit", function(event) {
-    if (!validarFormulario()) {
-        event.preventDefault(); // Evitar que se envíe el formulario si la validación falla
-    }
-});
